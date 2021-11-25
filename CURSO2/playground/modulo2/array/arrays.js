4f7bfb1d169c8b72d608c28d9e48b46cf276e841
JGusM <jgusmei@gmail.com>
2021-11-25 18:08:04 -0300

    movi la carpeta arrasy dentro de modulo 2

---
+++ CURSO2/playground/modulo2/array/arrays.js
@@ -0,0 +1,18 @@
+// Arrays
+
+let nombres = [ "Nacho", "Darío", "Javier"];
+let edades = [ 34, 28, 37, 40, 43 ];
+let valoresDeVerdad = [ true, false, false ];
+let variados = [ "Nacho", 34, true, false, edades ];
+
+// Un dato concreto dentro de un array
+
+console.log(edades);
+console.log(valoresDeVerdad);
+
+// ¿Arrays dentro de un arrays?
+
+console.log(variados[4][1]);
+
+
+
