599e14e03645370decf7516bb1571bd9821a3b9d
JGusM <jgusmei@gmail.com>
2021-11-25 19:21:23 -0300

    metodos de string length, indexOf, replace, split, slice, trim

---
+++ CURSO2/playground/modulo2/metodosdestring/metodos-string.js
@@ -0,0 +1,36 @@
+/*
+// Ejemplo de length
+
+let mensaje = 'Gran día para practicar Javascript!';
+console.log(mensaje.length);// devuelve la cantidad de elementos del string
+
+ // Ejemplo de indexOf
+console.log(mensaje.indexOf('día'));//devuelve el nro de indice que se encuentra el elemento
+
+// Ejemplo de slice
+console.log(mensaje.slice(0, 24));// muestra el recorte desde el 0 al 24
+console.log(mensaje); //aca muestra que el mensaje original sigue igual
+*/
+/*
+let mensaje = 'Gran día para practicar Javascript!';
+console.log(mensaje);
+
+// Ejemplo de trim
+console.log(mensaje.trim());// con trim recorta espacios iniciales si los tuviera
+
+// Ejemplo de split
+console.log(mensaje.split(" "));// aca va a devolver un array separando el string por espacios vacios
+
+// Ejemplo de replace
+console.log(mensaje.replace('Javascript', 'todo'));// aca va a reemplazar Javascript por todo
+console.log(mensaje);// aca muestra que el mensaje original sigue igual
+
+let nombre = 'gustavo';
+console.log(nombre[4])//aca muestra el elemento del indice 4 que es la a
+*/
+/*
+let frase = 'Breaking Bad Rules';
+console.log(frase.length);// aca muestra la cantidad de elementos que tiene el string
+console.log(frase.slice(-10, -6));// aca recorta de atras para adelante y devuelve Bad
+console.log(frase.slice(13, 18));// aca recorta Rules
+*/
