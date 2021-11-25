e22ba8c345a7c5a13ee1c37ff66159ecd6a469b9
JGusM <jgusmei@gmail.com>
2021-11-25 19:48:14 -0300

    agregue ejercicios de playground y uno de includes

--- CURSO2/playground/modulo2/metodosdestring/metodos-string.js
+++ CURSO2/playground/modulo2/metodosdestring/metodos-string.js
@@ -34,3 +34,65 @@ console.log(frase.length);// aca muestra la cantidad de elementos que tiene el s
 console.log(frase.slice(-10, -6));// aca recorta de atras para adelante y devuelve Bad
 console.log(frase.slice(13, 18));// aca recorta Rules
 */
+/*
+Completar URL
+Debemos crear una función llamada dominio que recibirá un string como "digitalhouse.com.ar" y su función será retornar: "http://www.digitalhouse.com.ar".
+
+function dominio (string){
+return "http://www." + string
+}
+*/
+
+/*
+Contar los caracteres
+Lo primero que necesitamos es declarar una variable llamada texto, asignándole como valor un texto (string), el que queramos. Luego, utilizando la propiedad length, tendremos que imprimir en consola el total de caracteres que contiene la frase.
+
+let texto = ("el texto que queramos");
+console.log(texto.length)
+*/
+
+/*
+Reemplazo fast fast
+Nuestra misión, ahora, es crear una función llamada reemplazoFastFast que va a recibir los siguientes tres parámetros:
+
+1. Un texto.
+
+2. La palabra que vamos a buscar para reemplazar.
+
+3. La palabra que vamos a usar para reemplazar.
+
+La función deberá devolver el texto con la palabra reemplazada.
+
+Ejemplo:
+
+let textoSinGuiones = reemplazoFastFast('Este texto es mala onda','mala','buena');
+console.log(textoSinGuiones) //'Este texto es buena onda'
+
+function reemplazoFastFast (string, buscar, reemplazar){
+    return string.replace(buscar, reemplazar)
+}
+console.log(reemplazoFastFast("Es un dia lluvioso", "lluvioso", "hermoso"));
+*/
+
+/*
+
+¿Están hablando de mí?
+Para el siguiente ejercicio vamos a suponer que tenemos un texto que copiamos y queremos saber si el texto menciona una palabra en particular.
+Para lograrlo, tendremos que crear una función llamada menciona, que recibirá dos parámetros: un texto —que es donde vamos a buscar— y una palabra —que es la que vamos a averiguar si se menciona en el texto—. La misma función retorna true en el caso de encontrar la palabra.
+Ejemplo:
+menciona('Existen muchos lenguajes de programación y JavaScript es uno de ellos','programación'); // true
+
+function menciona ( string , palabra){
+    return string.includes(palabra)
+}
+console.log(menciona("estan hablando de mi", "hablando"));//aca devuelve true porque encontro la palabra que buscaba
+*/
+
+/*
+Solo el nombre
+Teniendo como punto de partida la cadena de texto '¡Hola!, soy Carli', deberemos recortarla para poder obtener el nombre 'Carli' en una nueva variable llamada licenciada.
+IMPORTANTE: Para la resolución tendremos que usar la función slice().
+
+let frase = 'Hola!, soy Carli';
+let licenciada = frase.slice(11, 16);// aca recorta Carli
+*/
