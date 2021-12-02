7e834aff561a19b3f54aa29e4d0e8f9d84f32f29
JGusM <jgusmei@gmail.com>
2021-12-02 10:07:31 -0300

    map reduce filter forEach

---
+++ CURSO2/playground/modulo2/metodosdearrays2/metodosdearrays2.js
@@ -0,0 +1,87 @@
+//----------Metodo Filter---------------
+
+/* 
+let estudiantes = [
+    {nombre: 'John', promedio: 8.5, aprobado: true},
+    {nombre: 'Jane', promedio: 7, aprobado: true},
+    {nombre: 'June', promedio: 3, aprobado: false},
+]
+
+let aprobados = estudiantes.filter(function(p){
+    return p.aprobado == true
+})
+let desaprobados = estudiantes.filter(function(estudiantes){
+    return estudiantes.aprobado == false
+})
+
+
+let aprobados = estudiantes.filter(p => p.aprobado == true);
+let desaprobados = estudiantes.filter(p => p.aprobado == false);
+
+console.log(aprobados);
+console.log(desaprobados);
+*/
+
+//----------Metodo Map---------------
+/* 
+let horariosPartida = [12, 14, 18, 21];
+
+let horariosAtrasados = horariosPartida.map(function(n){
+    return n - 1
+})
+console.log(horariosAtrasados);
+ */
+
+//----------Metodo Reduce---------------
+/* 
+let vueltas = [5, 8, 12, 3, 22]
+
+let totalVueltas = vueltas.reduce(function(acum, num){
+    return acum + num
+})
+console.log(totalVueltas);
+*/
+
+//----------Metodo forEach---------------
+/* 
+let listaDeSuperMercado = [
+    'Bife de chorizo', 
+    'Coca Cola', 
+    'Bananas', 
+    'Lechuga', 
+    'Chimichurri', 
+    'Lata de tomates', 
+    'Arvejas', 
+    'Cereales', 
+    'Pechuga de pollo', 
+    'Leche'
+];
+
+listaDeSuperMercado.forEach(function(item){
+    console.log(item);
+})
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
