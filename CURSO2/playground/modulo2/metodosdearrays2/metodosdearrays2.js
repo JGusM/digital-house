8a744da175a026879ebfe7faed06943c29c2c29d
JGusM <jgusmei@gmail.com>
2021-12-18 21:26:54 -0300

    actualizacion archivo

--- CURSO2/playground/modulo2/metodosdearrays2/metodosdearrays2.js
+++ CURSO2/playground/modulo2/metodosdearrays2/metodosdearrays2.js
@@ -1,6 +1,6 @@
 //----------Metodo Filter---------------
 
-/* 
+
 let estudiantes = [
     {nombre: 'John', promedio: 8.5, aprobado: true},
     {nombre: 'Jane', promedio: 7, aprobado: true},
@@ -15,12 +15,12 @@ let desaprobados = estudiantes.filter(function(estudiantes){
 })
 
 
-let aprobados = estudiantes.filter(p => p.aprobado == true);
-let desaprobados = estudiantes.filter(p => p.aprobado == false);
+// let aprobados = estudiantes.filter(p => p.aprobado == true);
+// let desaprobados = estudiantes.filter(p => p.aprobado == false);
 
 console.log(aprobados);
 console.log(desaprobados);
-*/
+
 
 //----------Metodo Map---------------
 /* 
@@ -30,7 +30,7 @@ let horariosAtrasados = horariosPartida.map(function(n){
     return n - 1
 })
 console.log(horariosAtrasados);
- */
+*/
 
 //----------Metodo Reduce---------------
 /* 
@@ -62,14 +62,56 @@ listaDeSuperMercado.forEach(function(item){
 })
  */
 
+//-------------------------------------------------
+/* 
+let estudiantes = [
+    {nombre: 'John', promedio: 8.5, aprobado: true},
+    {nombre: 'Jane', promedio: 7, aprobado: true},
+    {nombre: 'June', promedio: 3, aprobado: false},
+]
+
+
+let autoEncontrado = estudiantes.filter(function(p){
+    return p.promedio < 5
+})
+
+console.log(autoEncontrado);
+ */
+
+/* 
+let autos = [
+    {
+        marca: 'Ford',
+        modelo: 'Fiesta',
+        color: 'Azul',
+        anio: 2019,
+        km: 200,
+        precio: 150000,
+        cuotas: 12,
+        patente: 'APL123',
+        vendido: false
+    },
+    {
+        marca: 'Toyota',
+        modelo: 'Corolla',
+        color: 'Blanco',
+        anio: 2019,
+        km: 0,
+        precio: 100000,
+        cuotas: 14,
+        patente: 'JJK116',
+        vendido: false
+    }
+]
 
 
 
+let autoEncontrado = autos.filter(function(p){
+    return p.patente == 'JJK116'
+})
 
-
-
-
-
+console.log(autoEncontrado);
+ */
 
 
 
