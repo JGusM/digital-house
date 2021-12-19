43821a37ee95e022984313fa6b08cf8a0e6f0dc5
JGusM <jgusmei@gmail.com>
2021-12-18 21:38:00 -0300

    repaso

---
+++ CURSO2/2021-12-02/repaso.js
@@ -0,0 +1,8 @@
+let array  = ["www.google.com", "www.facebook.com", "www.taringa.net"];
+
+let arrayNuevo = array.map((elemento) =>{
+    return elemento+"/inicio";
+})
+
+console.log(array)
+console.log(arrayNuevo)
