655208a6d02acf4202a9cd2ec41a1bf4c4558ded
JGusM <jgusmei@gmail.com>
2022-01-13 17:25:04 -0300

    Hice Form de Login y Registro

--- CURSO2/2021-12-21/mercadoliebre/app.js
+++ CURSO2/2021-12-21/mercadoliebre/app.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const path = require("path");
+const publicPath = path.resolve(__dirname, "./public");
+
 app.use( express.static('public'))
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
 })
 
 app.listen(3000, () => {
